8d798b791634b35d0d19a267521fa28a7a688cb0
mdpabel <mdpabel385@gmail.com>
2021-08-06 20:44:12 +0600

    sleep function

---
+++ sleep/sleep.js
@@ -0,0 +1,11 @@
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+console.log("Hello");
+sleep(2000);
+console.log("World!");
